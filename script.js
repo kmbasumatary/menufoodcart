05916efef831344670c697d2f0657cd2028e0344
kmbasumatary <155880975+kmbasumatary@users.noreply.github.com>
2024-01-24 23:52:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,42 +13,58 @@ async function addFoodItems() {
         return;
     }
     csvData.data.forEach((row, index) => {
-        if (row['FoodItem'] && row['Price'] && row['DiscountedPrice']) {
+        if (row['FoodItem'] && row['Price'] && row['DiscountedPrice'] && row['ItemsLeft']) {
             const div = document.createElement('div');
             div.className = 'food-item';
             div.style.animationDelay = `${index * 0.1}s`;
+
             const item = document.createElement('div');
             item.className = 'item';
+
             const foodName = document.createElement('span');
             foodName.textContent = row['FoodItem'];
             item.appendChild(foodName);
+
+            const itemsLeft = document.createElement('div');
+            itemsLeft.textContent = 'Items left: ' + row['ItemsLeft'];
+            item.appendChild(itemsLeft);
+
             const priceDiv = document.createElement('div');
             priceDiv.style.display = 'flex'; // Add this line to make the prices display side by side
+
             const price = document.createElement('span');
             price.textContent = '₹' + row['Price'];
             price.style.textDecoration = 'line-through'; // Strike out the old price
             priceDiv.appendChild(price);
+
             const discountedPrice = document.createElement('span');
             discountedPrice.textContent = '₹' + row['DiscountedPrice']; // Display the discounted price from the CSV file
             discountedPrice.style.color = 'red'; // Highlight the discounted price
             discountedPrice.style.marginLeft = '10px'; // Add some space between the discounted price 
             priceDiv.appendChild(discountedPrice);
+
             item.appendChild(priceDiv);
             div.appendChild(item);
+
             const counter = document.createElement('div');
             counter.className = 'counter';
+
             const minusButton = document.createElement('button');
             minusButton.textContent = '–';
             minusButton.setAttribute('aria-label', 'Decrease quantity');
+
             const countBox = document.createElement('span');
             countBox.textContent = '0';
+
             const plusButton = document.createElement('button');
             plusButton.textContent = '+';
             plusButton.setAttribute('aria-label', 'Increase quantity');
+
             counter.appendChild(minusButton);
             counter.appendChild(countBox);
             counter.appendChild(plusButton);
             div.appendChild(counter);
+
             document.getElementById('container').appendChild(div);
         }
     });
